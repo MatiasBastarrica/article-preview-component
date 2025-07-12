cb01d984d964b9f5e263e521aedcccf97fbe1c33
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2025-07-11 21:14:06 -0300

    Finish the desktop layout and change the modal position and design for this layout

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const shareIcons = document.querySelectorAll("button.share-icon-container");
 const shareOptions = document.querySelector(".sharing-modal");
+const btnDesktopContainer = document.querySelector(".btn-desktop");
+const btnDesktop = btnDesktopContainer.querySelector("button");
+const svgDesktop = btnDesktop.querySelector("svg");
+const pathDesktopSvg = svgDesktop.querySelector("path");
 
 function toggleShareOptions() {
   shareOptions.classList.toggle("hide");
@@ -7,4 +11,9 @@ function toggleShareOptions() {
 
 shareIcons.forEach((element) => {
   element.addEventListener("click", toggleShareOptions);
+});
+
+btnDesktop.addEventListener("click", () => {
+  btnDesktop.classList.toggle("btn-desktop-pressed-bg");
+  pathDesktopSvg.classList.toggle("btn-desktop-pressed-fill");
 });
